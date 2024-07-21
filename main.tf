ecc370c44430168368af04ad4c8eaff70a7d49ee
okuyama-code <ooyy0121@gmail.com>
2024-07-21 21:23:36 +0900

    コミット

--- main.tf
+++ main.tf
@@ -8,6 +8,12 @@ resource "aws_instance" "example" {
   # EC2（Elastic Compute Cloud）インスタンスタイプ
   instance_type = "t3.micro"
 
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+  EOF
+
   tags = {
     Name = "example"
   }
