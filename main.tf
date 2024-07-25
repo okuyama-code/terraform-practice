8ec4a1721ce618a8a9c7b4fa44405fb1dd45cfa5
okuyama-code <ooyy0121@gmail.com>
2024-07-26 07:50:19 +0900

    IAMポリシーを定義

--- main.tf
+++ main.tf
@@ -1,12 +1,28 @@
-module "web_server" {
-  source = "./http_server"
-  instance_type = "t3.micro"
+# ポリシードキュメント (権限)
+# これは単なる説明や設計図ではなく、AWS環境で直接機能する実行可能な設定です。このJSONを適切な場所に適用することで、実際のアクセス制御が実装されます。
+# 下のaws_iam_policy_documentと同じないようなのでコメントアウト
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": ["ec2:DescribeRegions"],
+#       "Resource": ["*"]
+#     }
+#   ]
+# }
+
+# ポリシードキュメントの定義
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:DescribeRegions"] # リージョンの一覧を取得する
+    resources = ["*"]
+  }
 }
 
-provider "aws" {
-  region = "ap-northeast-1"
-}
-
-output "public_dns" {
-  value = module.web_server.public_dns
+# IAMポリシーの定義
+resource "aws_iam_policy" "example" {
+  name = "example"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
